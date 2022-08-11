9f5f48e7c9905fd03ab04211abce191ee863969d
João Nogueira <joao.p.s.nog@gmail.com>
2022-08-11 22:59:20 +0100

    Cleaned configs

--- backend/src/app.spec.ts
+++ backend/src/app.spec.ts
@@ -4,7 +4,7 @@ import { QUESTIONS } from './data/question.data';
 
 describe("Testing the endpoints", () => {
 
-    test("It should get the questions with a get", done => {
+    it("It should get the questions with a get", done => {
       request(app)
         .get("/questions")
         .then(response => {
@@ -14,14 +14,14 @@ describe("Testing the endpoints", () => {
         });
     });
 
-    test("It should post the answers and get the results", done => {
+    it("It should post the answers and get the results", done => {
       const requestBody = {
         answers: [1,2,3,4]
       };
       const expectedResponse = {
         result: 10
       }
-
+      
       request(app)
         .post("/result").send(requestBody)
         .then(response => {
